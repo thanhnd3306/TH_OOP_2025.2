0022cbaac587e70f6872db529bdf8989e4fa4b5c
Thanhnd <thanhnd3306@gmail.com>
2026-04-20 23:13:06 +0700

    Add overloaded addDigitalVideoDisc methods

--- AimsProject/Cart.java
+++ AimsProject/Cart.java
@@ -13,6 +13,17 @@ public class Cart {
         }
     }
 
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (int i = 0; i < dvdList.length; i++) {
+            addDigitalVideoDisc(dvdList[i]);
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
+    }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         boolean found = false;
         for (int i = 0; i < qtyOrdered; i++) {
